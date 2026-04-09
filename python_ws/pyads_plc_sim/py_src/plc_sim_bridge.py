24ef8062ee03c341a1122f0446be579a47fbec68
Anto Ronson <antoronson590@gmail.com>
2026-04-09 07:32:41 +0200

    working code: After bug fixing and running around, the input from python can be read from plc.

--- python_ws/pyads_plc_sim/py_src/plc_sim_bridge.py
+++ python_ws/pyads_plc_sim/py_src/plc_sim_bridge.py
@@ -11,11 +11,21 @@ class PlcSimBridge:
         self.plc = plc_connector(target_ams, target_port)
         self.wait_for_plc()          # ← just WAIT, don't connect (watchdog does it)
         self.signals = PLC_Signals
-        self.io_data = {}
-        all_keys = list(self.signals.MAPPING_PLC_INPUTS.keys()) + \
-            list(self.signals.MAPPING_PLC_OUTPUTS.keys())
-        for key in all_keys:
-            self.io_data[key] = None
+
+        ##################################
+        # We separate input and output
+        # to handle it better
+        ##################################
+        self.input_data = {}
+        self.output_data = {}
+        self.input_keys = list(self.signals.MAPPING_PLC_INPUTS.keys())
+        self.output_keys = list(self.signals.MAPPING_PLC_OUTPUTS.keys())
+        for key in self.input_keys:
+            self.logger.info(f"Input Key: {key}")
+            self.input_data[key] = None
+        for key in self.output_keys:
+            self.logger.info(f"Output Key: {key}")
+            self.output_data[key] = None
 
     def wait_for_plc(self):
         """Wait until the watchdog establishes connection."""
@@ -31,7 +41,7 @@ class PlcSimBridge:
             for key, config in self.signals.MAPPING_PLC_OUTPUTS.items():
                 try:
                     val = self.plc.read_variable(config["plc"], config["type"])
-                    self.io_data[key] = val
+                    self.output_data[key] = val
                 except Exception as e:
                     self.logger.error(f"Failed to sync input {key}: {e}")
         else:
@@ -41,7 +51,7 @@ class PlcSimBridge:
         self.logger.info("Writing to PLC")
         if self.plc.isAlive:
             for key, config in self.signals.MAPPING_PLC_INPUTS.items():
-                val = self.io_data.get(key)
+                val = self.input_data.get(key)
                 if val is not None:
                     try:
                         self.plc.write_variable(
@@ -58,8 +68,48 @@ class PlcSimBridge:
         self.read_from_plc()
 
         # TODO: do the action block here
-        self.io_data["conveyor_is_moving"] = self.io_data.get(
-            "move_conveyor")   # ← typo fixed
-        self.io_data["conveyor_is_stopped"] = self.io_data.get("stop_conveyor")
+        self.set_key_value("conveyor_is_moving",
+                           self.get_key_value("move_conveyor"))
+        self.set_key_value("conveyor_is_stopped",
+                           self.get_key_value("stop_conveyor"))
+
+        for key in self.input_keys:
+            if self.get_key_value(key) is None:
+                self.set_key_value(key, False)
 
         self.write_to_plc()
+
+    def get_key_value(self, key):
+        if key in self.input_keys:
+            self.logger.info(f"{key} is found in input key list")
+            try:
+                value = self.input_data[key]
+                return value
+            except Exception as e:
+                self.logger.error(
+                    f"Error while reading the input key value {key}: {e}")
+                return None
+
+        if key in self.output_keys:
+            self.logger.info(f"{key} is found in output key  list")
+            try:
+                value = self.output_data[key]
+                return value
+            except Exception as e:
+                self.logger.error(
+                    f"Error while reading the output key value {key}: {e}")
+                return None
+        self.logger.warn("Key {key} not in the list defined")
+        return None
+
+    def set_key_value(self, key, value):
+        # set key value only sets to output as it is logically not possible to set an input from plc
+        if key in self.input_keys:
+            try:
+                self.input_data[key] = value
+            except Exception as e:
+                self.logger.error(
+                    f"Error faced while writing {value} to the {key}: {e}")
+
+        else:
+            self.logger.warn(f"{key} is not a valid output key")
